8ac110a8223994dcbe04c865f62e118aaf23a9a3
BoltR6 <johnwallacewhite@gmail.com>
2024-02-29 09:39:54 -0600

    Command handling structure implemented.

--- src/main.rs
+++ src/main.rs
@@ -1,43 +1,91 @@
-use std::env;
+use std::{
+    env,
+    collections::{ VecDeque }
+};
 
 use serenity::async_trait;
-use serenity::prelude::*;
 use serenity::model::channel::Message;
-use serenity::framework::standard::macros::{command, group};
-use serenity::framework::standard::{StandardFramework, Configuration, CommandResult};
-
-#[group]
-#[commands(ping)]
-struct General;
+use serenity::model::gateway::Ready;
+use serenity::prelude::*;
 
 struct Handler;
 
+async fn econ( ctx: Context, msg: Message, args: VecDeque<String> ) {
+    todo!();
+}
+async fn stats( ctx: Context, msg: Message, args: VecDeque<String> ) {
+    todo!();
+}
+async fn opsec( ctx: Context, msg: Message, args: VecDeque<String> ) {
+    todo!();
+}
+async fn bans( ctx: Context, msg: Message, args: VecDeque<String> ) {
+    todo!();
+}
+async fn admin( ctx: Context, msg: Message, args: VecDeque<String> ) {
+    todo!();
+}
+async fn help( ctx: Context, msg: Message, args: VecDeque<String> ) {
+    todo!();
+}
+
 #[async_trait]
-impl EventHandler for Handler {}
+impl EventHandler for Handler {
+    async fn message(&self, ctx: Context, msg: Message) {
+        let mut args: VecDeque<String> = msg.content
+            .clone()
+            .split(' ')
+            .map(|i| String::from(i))
+            .collect();
 
-#[tokio::main]
-async fn main() {
-    let framework = StandardFramework::new().group(&GENERAL_GROUP);
-    framework.configure(Configuration::new().prefix("~")); // set the bot's prefix to "~"
+        if args.pop_front() != Some(String::from("r6")) {
+            return;
+        }
 
-    // Login with a bot token from the environment
-    let token = env::var("DISCORD_TOKEN").expect("token");
-    let intents = GatewayIntents::non_privileged() | GatewayIntents::MESSAGE_CONTENT;
-    let mut client = Client::builder(token, intents)
-        .event_handler(Handler)
-        .framework(framework)
-        .await
-        .expect("Error creating client");
 
-    // start listening for events by starting a single shard
-    if let Err(why) = client.start().await {
-        println!("An error occurred while running the client: {:?}", why);
+        match args
+            .pop_front()
+            .unwrap_or(String::from("help"))
+            .as_str()
+        {
+            "econ" => { tokio::spawn(econ(ctx, msg, args)); },
+            "stats" => { tokio::spawn(stats(ctx, msg, args)); },
+            "opsec" => { tokio::spawn(opsec(ctx, msg, args)); },
+            "bans" => { tokio::spawn(opsec(ctx, msg, args)); },
+            "admin" => { tokio::spawn(admin(ctx, msg, args)); },
+            _ => { tokio::spawn(help(ctx, msg, args)); }
+        }
+    }
+
+    // Set a handler to be called on the `ready` event. This is called when a shard is booted, and
+    // a READY payload is sent by Discord. This payload contains data like the current user's guild
+    // Ids, current user data, private channels, and more.
+    //
+    // In this case, just print what the current user's username is.
+    async fn ready(&self, _: Context, ready: Ready) {
+        println!("{} is connected!", ready.user.name);
     }
 }
 
-#[command]
-async fn ping(ctx: &Context, msg: &Message) -> CommandResult {
-    msg.reply(ctx, "Pong!").await?;
+#[tokio::main]
+async fn main() {
+    // Configure the client with your Discord bot token in the environment.
+    let token = env::var("DISCORD_BOT_TOKEN").expect("Expected a token in the environment");
+    // Set gateway intents, which decides what events the bot will be notified about
+    let intents = GatewayIntents::GUILD_MESSAGES
+        | GatewayIntents::DIRECT_MESSAGES
+        | GatewayIntents::MESSAGE_CONTENT;
 
-    Ok(())
+    // Create a new instance of the Client, logging in as a bot. This will automatically prepend
+    // your bot token with "Bot ", which is a requirement by Discord for bot users.
+    let mut client =
+        Client::builder(&token, intents).event_handler(Handler).await.expect("Err creating client");
+
+    // Finally, start a single shard, and start listening to events.
+    //
+    // Shards will automatically attempt to reconnect, and will perform exponential backoff until
+    // it reconnects.
+    if let Err(why) = client.start().await {
+        println!("Client error: {why:?}");
+    }
 }
