5951e85233cd4b8338ae1405453f9d8cc78d0aec
BoltR6 <johnwallacewhite@gmail.com>
2024-03-22 19:43:45 -0500

    Boilerplate for analyze command

--- src/econ/mod.rs
+++ src/econ/mod.rs
@@ -7,6 +7,14 @@ use crate::Arc;
 use crate::unimplemented;
 use crate::send_embed;
 
+async fn name_or_item_id( state: Arc<Mutex<State>>, unknown_id: String ) -> String {
+    if unknown_id.chars().filter(|&ch| ch.is_ascii_digit() ).count() > 5 {
+        println!("probably an id");
+        return unknown_id;
+    }
+    println!("{:?}", state.lock().await.market_data);
+    unknown_id
+}
 async fn list( state: Arc<Mutex<State>>, mut args: VecDeque<String> ) -> String {
     // Get the page number
     let page: usize = args.pop_front()
@@ -28,6 +36,19 @@ async fn list( state: Arc<Mutex<State>>, mut args: VecDeque<String> ) -> String 
 
     msg
 }
+async fn data( state: Arc<Mutex<State>>, mut args: VecDeque<String> ) -> Result<String, String> {
+    let mut msg: String = format!("");
+
+    let item_id = name_or_item_id(
+        state.clone(),
+        args.pop_front()
+            .ok_or(String::from("Missing last argument `item id`!"))?
+    ).await;
+
+    println!("");
+
+    Ok(msg)
+}
 pub async fn econ( state: Arc<Mutex<State>>, ctx: Context, msg: Message, mut args: VecDeque<String> ) {
     match args
         .pop_front()
@@ -36,6 +57,20 @@ pub async fn econ( state: Arc<Mutex<State>>, ctx: Context, msg: Message, mut arg
     {
         "list" => {
             let result: String = list( state, args ).await;
+
+            send_embed(
+                &ctx, 
+                &msg, 
+                "Tracked Skins", 
+                &result, 
+                "https://github.com/hiibolt/hiibolt/assets/91273156/4a7c1e36-bf24-4f5a-a501-4dc9c92514c4"
+            ).await
+                .unwrap();
+        },
+        "analyze" => {
+            let result: String = data( state, args )
+                .await
+                .unwrap_or_else(|err| err);
 
             send_embed(
                 &ctx, 
